dc8a6a42c20542de28bba0d7ab5d3739db12c73f
mr-dj06 <mr.learner06@gmail.com>
2025-06-14 20:02:08 +0530

    updated code

---
+++ client/src/routes/About/about.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is QuickNotes </p>
+      <p>A simple and efficient notes application built with React for the frontend and Node.js, Express, and MongoDB for the backend. </p>
+      {/* <p>
+        This app allows users to create, read, update, and delete notes. 
+      </p>
+      <p>
+        The backend is powered by Node.js and Express, with MongoDB serving as the database for storing notes. The frontend is designed with React to provide a dynamic and user-friendly interface.
+      </p>
+      <p>
+        The app offers a smooth and responsive experience for managing personal notes, showcasing fundamental Full-Stack development skills.
+      </p> */}
+    </div>
+  )
+}
+
+export default About
